0bba863324f4ef157ec1ef589591fc5860e944c1
pippsza <bastahov18@gmail.com>
2025-07-11 01:38:11 +0300

    new bug

--- src/components/BackCircles/BackCircles.jsx
+++ src/components/BackCircles/BackCircles.jsx
@@ -2,8 +2,8 @@ import Container from "../Container/Container.jsx";
 
 export default function BackCircles() {
   return (
-    <Container style=" ">
-      <div className="-z-10 absolute bg-green-700 w-full h-full"></div>
+    <Container style=" absolute -z-10 top-0 left-1/2 h-full transform -translate-x-1/2">
+      <div className="bg-green-700 w-full h-full"></div>
     </Container>
   );
 }
